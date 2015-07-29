c8891c4627663c4a120475b3ebb7077125f226f5
hcz <h.cz@qq.com>
2015-07-29 13:36:36 +0800

    init

---
+++ mese_core.cpp
@@ -0,0 +1,200 @@
+#include "mese_core.hpp"
+
+void MESE::exec() {
+    player_count = last.player_count;
+    now_period = last.now_period + 1;
+
+    for (int i = 0; i < player_count; ++i) {
+        deprecation[i] = last.capital[i] * setting.deprecation_rate;
+        capital[i] = last.capital[i] + decision.ci[i] - deprecation[i];
+        size[i] = capital[i] / setting.unit_fee;
+
+        prod_rate[i] = decision.prod[i] / last.size[i];
+        prod_cost_unit[i] = (
+            setting.prod_cost_factor_rate
+                * (prod_rate[i] > 0.8 ? 0.5 : 1) * pow(prod_rate[i] - 0.8, 2)
+            + setting.prod_cost_factor_size
+                / player_count / last.capital[i]
+            + setting.prod_cost_factor_const
+        );
+        prod_cost_total[i] = prod_cost_unit[i] * decision.prod[i];
+
+        goods[i] = decision.prod[i] + last.inventory[i];
+        goods_max_sales[i] = decision.price[i] * goods[i];
+
+        history_mk[i] = last.history_mk[i] + decision.mk[i];
+        history_rd[i] = last.history_rd[i] + decision.rd[i];
+    }
+
+    sum_mk = sum(decision.mk);
+    sum_mk_compressed = (
+        sum_mk > setting.mk_overload ? (
+            (sum_mk - setting.mk_overload) * setting.mk_compression
+            + setting.mk_overload
+        ) : sum_mk
+    );
+    sum_history_mk = sum(history_mk);
+    sum_history_rd = sum(history_rd);
+
+    average_price_given = sum(decision.price) / player_count;
+    average_price_planned = div(
+        sum(goods_max_sales), sum(goods), average_price_given
+    );
+    average_price_mixed = (
+        setting.demand_price * average_price_planned
+        + (1 - setting.demand_price) * last.average_price
+    );
+
+    demand_effect_mk = setting.demand_mk * (
+        sqrt(sum_mk_compressed) / average_price_mixed
+    ); // 5.3
+    demand_effect_rd = setting.demand_rd * (
+        sum_history_rd / (now_period + 1)
+    ); // 2.66666
+    orders_demand = setting.demand * (
+        demand_effect_rd + demand_effect_mk
+    );
+
+    for (int i = 0; i < player_count; ++i) {
+        share_effect_price[i] = pow(
+            average_price_mixed / decision.price[i],
+            setting.share_pow_price
+        );
+        share_effect_mk[i] = pow(
+            decision.mk[i] / decision.price[i],
+            setting.share_pow_mk
+        );
+        share_effect_rd[i] = pow(
+            history_rd[i],
+            setting.share_pow_rd
+        );
+    }
+
+    sum_share_effect_price = sum(share_effect_price);
+    sum_share_effect_mk = sum(share_effect_mk);
+    sum_share_effect_rd = sum(share_effect_rd);
+
+    for (int i = 0; i < player_count; ++i) {
+        // orders
+
+        share[i] = (
+            setting.share_price * div(share_effect_price[i], sum_share_effect_price, 0)
+            + setting.share_mk * div(share_effect_mk[i], sum_share_effect_mk, 0)
+            + setting.share_rd * div(share_effect_rd[i], sum_share_effect_rd, 0)
+        );
+
+        share_compressed[i] =  (
+            decision.price[i] > setting.price_overload ? (
+                share[i] * setting.price_overload / decision.price[i]
+            ) : share[i]
+        );
+
+        orders[i] = orders_demand * share[i];
+        sold[i] = (
+            (orders[i] > goods[i]) ?
+            goods[i] :
+            orders[i]
+        );
+        inventory[i] = goods[i] - sold[i];
+        unfilled[i] = orders[i] - sold[i];
+
+        // production and goods
+
+        goods_cost[i] = last.goods_cost_inventory[i] + prod_cost_total[i];
+        goods_cost_sold[i] = goods_cost[i] * div(sold[i], goods[i], 0);
+        goods_cost_inventory[i] = goods_cost[i] - goods_cost_sold[i];
+
+        // cash flow
+
+        spending[i] = (
+            prod_cost_total[i]
+            + decision.ci[i] - deprecation[i]
+            + decision.mk[i] + decision.rd[i]
+        );
+        balance_early[i] = last.cash[i] - last.loan[i] - spending[i];
+        loan_early[i] = max(- balance_early[i], 0);
+
+        sales[i] = decision.price[i] * sold[i];
+
+        interest[i] = (
+            (last.cash[i] > 0 && last.loan[i] == 0) ?
+            - setting.interest_rate_cash * last.cash[i] :
+            setting.interest_rate_loan * loan_early[i] /* last.loan[i] */
+        );
+        inventory_charge[i] = (
+            last.inventory[i] < inventory[i] ?
+            last.inventory[i] :
+            inventory[i]
+        ) * setting.inventory_fee;
+
+        cost_before_tax[i] = (
+            prod_cost_total[i]
+            + deprecation[i]
+            + decision.mk[i] + decision.rd[i]
+            + interest[i] + inventory_charge[i]
+        );
+        profit_before_tax[i] = sales[i] - cost_before_tax[i];
+        tax_charge[i] = profit_before_tax[i] * setting.tax_rate;
+        profit[i] = profit_before_tax[i] - tax_charge[i];
+
+        balance[i] = (
+            last.cash[i] + loan_early[i] - last.loan[i]
+            + profit[i]
+            - decision.ci[i] + deprecation[i]
+            + goods_cost_sold[i] - prod_cost_total[i]
+        );
+        loan[i] = max(loan_early[i], loan_early[i] - balance[i]);
+        cash[i] = max(balance[i], 0);
+        retern[i] = last.retern[i] + profit[i];
+    }
+
+    sum_size = sum(size);
+    sum_sold = sum(sold);
+    sum_sales = sum(sales);
+
+    for (int i = 0; i < player_count; ++i) {
+        mpi_a[i] = round(
+            setting.mpi_factor_a * player_count * (
+                retern[i] / (now_period + 1)
+                / setting.mpi_retern_factor
+            )
+        );
+
+        mpi_b[i] = round(
+            setting.mpi_factor_b * player_count * (
+                (history_rd[i] + history_mk[i])
+                / (sum_history_rd + sum_history_mk)
+            )
+        );
+
+        mpi_c[i] = round(
+            setting.mpi_factor_c * player_count * (
+                size[i] / sum_size
+            )
+        );
+
+        mpi_d[i] = round(
+            setting.mpi_factor_d * (
+                1 - fabs(80 - prod_rate[i])
+            )
+        );
+
+        mpi_e[i] = round(
+            setting.mpi_factor_e * player_count * div(
+                sold[i], sum_sold, 0
+            )
+        );
+
+        mpi_f[i] = min(round(
+            setting.mpi_factor_f * div(
+                div(sold[i], last.sold[i], 0),
+                div(sum_sales, last.sum_sales, 0),
+                0
+            )
+        ), 20);
+
+        mpi[i] = mpi_a[i] + mpi_b[i] + mpi_c[i] + mpi_d[i] + mpi_e[i] + mpi_f[i];
+    }
+
+    average_price = div(sum(sales), sum(sold), average_price_given);
+}
---
+++ mese_core.hpp
@@ -0,0 +1,174 @@
+#pragma once
+
+#include <cmath>
+#include <string>
+
+struct MESE {
+    static const size_t BUFSIZE = 32;
+
+    MESE &last;
+
+    size_t player_count;
+    size_t now_period;
+    std::string company_name[BUFSIZE]; // TODO
+
+    struct {
+        // decision limits
+
+        double price_limit;
+        double loan_limit;
+        double mk_limit;
+        double ci_limit;
+        double rd_limit;
+
+        // costs related
+
+        double prod_cost_factor_rate; // 138
+        double prod_cost_factor_size; // 2520000 = 168000 * 15
+        double prod_cost_factor_const; // 3
+
+        double deprecation_rate; // 0.05
+
+        double interest_rate_cash; // 0.005 - 0.0001 * setting
+        double interest_rate_loan; // 0.0025 + 0.0001 * setting
+        double inventory_fee; // 1
+        double unit_fee; // 40
+        double tax_rate; // 0.01 * setting
+
+        // orders related
+
+        double mk_overload; // 16800
+        double mk_compression; // 0.25
+
+        double demand; // 500 + setting
+        double demand_price; // 1 + 0.01 * setting
+        double demand_mk; // 159 / sqrt(8400) + 0.0053 * setting
+        double demand_rd; // (1 + 0.01 * setting) / 3150
+
+        double share_price; // 0.01 * setting
+        double share_mk; // 0.01 * setting
+        double share_rd; // 0.01 * setting
+
+        double share_pow_price; // 3
+        double share_pow_mk; // 1.5
+        double share_pow_rd; // 1
+
+        double price_overload; // 40
+
+        // mpi related
+
+        double mpi_retern_factor; // 11155
+        double mpi_factor_a; // 50
+        double mpi_factor_b; // 10
+        double mpi_factor_c; // 10
+        double mpi_factor_d; // 10
+        double mpi_factor_e; // 10
+        double mpi_factor_f; // 10
+    } setting;
+
+    struct {
+        double price[BUFSIZE];
+        double prod[BUFSIZE];
+        double mk[BUFSIZE];
+        double ci[BUFSIZE];
+        double rd[BUFSIZE];
+    } decision;
+
+    double deprecation[BUFSIZE];
+    double capital[BUFSIZE];
+    double size[BUFSIZE];
+
+    double prod_rate[BUFSIZE];
+    double prod_cost_unit[BUFSIZE];
+    double prod_cost_total[BUFSIZE];
+    double goods[BUFSIZE];
+    double goods_max_sales[BUFSIZE];
+
+    double history_mk[BUFSIZE];
+    double history_rd[BUFSIZE];
+
+    double sum_mk;
+    double sum_mk_compressed;
+    double sum_history_mk;
+    double sum_history_rd;
+
+    double average_price_given;
+    double average_price_planned;
+    double average_price_mixed;
+    double demand_effect_mk;
+    double demand_effect_rd;
+    double orders_demand;
+
+    double share_effect_price[BUFSIZE];
+    double share_effect_mk[BUFSIZE];
+    double share_effect_rd[BUFSIZE];
+    double sum_share_effect_price;
+    double sum_share_effect_mk;
+    double sum_share_effect_rd;
+    double share[BUFSIZE];
+    double share_compressed[BUFSIZE];
+
+    double orders[BUFSIZE];
+    double sold[BUFSIZE];
+    double inventory[BUFSIZE];
+    double unfilled[BUFSIZE];
+
+    double goods_cost[BUFSIZE];
+    double goods_cost_sold[BUFSIZE];
+    double goods_cost_inventory[BUFSIZE];
+
+    double spending[BUFSIZE];
+    double balance_early[BUFSIZE];
+    double loan_early[BUFSIZE];
+
+    double sales[BUFSIZE];
+    double interest[BUFSIZE];
+    double inventory_charge[BUFSIZE];
+    double cost_before_tax[BUFSIZE];
+    double profit_before_tax[BUFSIZE];
+    double tax_charge[BUFSIZE];
+    double profit[BUFSIZE];
+
+    double balance[BUFSIZE];
+    double loan[BUFSIZE];
+    double cash[BUFSIZE];
+    double retern[BUFSIZE];
+
+    double sum_size;
+    double sum_sold;
+    double sum_sales;
+
+    double mpi_a[BUFSIZE];
+    double mpi_b[BUFSIZE];
+    double mpi_c[BUFSIZE];
+    double mpi_d[BUFSIZE];
+    double mpi_e[BUFSIZE];
+    double mpi_f[BUFSIZE];
+    double mpi[BUFSIZE];
+
+    double average_price;
+
+    inline double sum(double (&member)[BUFSIZE]) {
+        double result = 0;
+
+        for (size_t i = 0; i < player_count; ++i) {
+            result += member[i];
+        }
+
+        return result;
+    }
+
+    inline double div(double a, double b, double error) {
+        return b == 0 ? error : a / b;
+    }
+
+    inline double min(double a, double b) {
+        return a > b ? b : a;
+    }
+
+    inline double max(double a, double b) {
+        return a > b ? a : b;
+    }
+
+    void exec();
+};
