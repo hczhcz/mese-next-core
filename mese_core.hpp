bcfbf8c2fc005e5c6427c2e39d1d49a6a7120c95
hcz <h.cz@qq.com>
2015-07-29 21:57:19 +0800

    add macros and working

--- mese_core.hpp
+++ mese_core.hpp
@@ -6,147 +6,154 @@
 
 namespace mese {
 
-const size_t MAX_PLAYER{16};
-const double FILL{0.0/0.0};
+#define MESE_VAL(name) double name {NAN}
+#define MESE_ARR(name) double name[16] { \
+    NAN, NAN, NAN, NAN, NAN, NAN, NAN, NAN, \
+    NAN, NAN, NAN, NAN, NAN, NAN, NAN, NAN, \
+}
+
+const size_t MAX_PLAYER {16};
 
 struct Setting {
     // decision limits
 
-    double price_max{99};
-    double price_min{12};
-    double mk_limit{15000};
-    double ci_limit{15000};
-    double rd_limit{15000};
-    double loan_limit{30000};
+    double price_max {99};
+    double price_min {12};
+    double mk_limit {15000 * 8}; // ref: on 8 player game
+    double ci_limit {15000 * 8}; // ref: on 8 player game
+    double rd_limit {15000 * 8}; // ref: on 8 player game
+    double loan_limit {30000 * 8}; // ref: on 8 player game
 
     // costs related
 
-    double prod_rate_balanced{0.8};
-    double prod_rate_pow{2};
-    double prod_cost_factor_rate_over{69};
-    double prod_cost_factor_rate_under{138};
-    double prod_cost_factor_size{168000 * 15};
-    double prod_cost_factor_const{3};
+    double prod_rate_balanced {0.8};
+    double prod_rate_pow {2};
+    double prod_cost_factor_rate_over {69};
+    double prod_cost_factor_rate_under {138};
+    double prod_cost_factor_size {15};
+    double prod_cost_factor_const {3};
 
-    double deprecation_rate{0.05};
+    double initial_cash {14700};
+    double initial_capital {168000};
+    double deprecation_rate {0.05};
 
-    double interest_rate_cash{0.0025}; // 0.005 - 0.0001 * setting
-    double interest_rate_loan{0.005}; // 0.0025 + 0.0001 * setting
-    double inventory_fee{1};
-    double unit_fee{40};
-    double tax_rate{0.1}; // 0.01 * setting
+    double interest_rate_cash {0.0025}; // 0.005 - 0.0001 * setting
+    double interest_rate_loan {0.005}; // 0.0025 + 0.0001 * setting
+    double inventory_fee {1};
+    double unit_fee {40};
+    double tax_rate {0.1}; // 0.01 * setting
 
     // orders related
 
-    double mk_overload{16800};
-    double mk_compression{0.25};
+    double mk_overload {16800};
+    double mk_compression {0.25};
 
-    double demand{500}; // 500 + setting
-    double demand_price{1}; // 1 + 0.01 * setting
-    double demand_mk{5.3}; // 159 / sqrt(8400) + 0.0053 * setting
-    double demand_rd{1}; // (1 + 0.01 * setting) / 3150
+    double demand {500}; // 500 + setting
+    double demand_price {1}; // 1 + 0.01 * setting
+    double demand_mk {5.3}; // 159 / sqrt(8400) + 0.0053 * setting
+    double demand_rd {1}; // (1 + 0.01 * setting) / 3150
 
-    double demand_ref_price{30};
-    double demand_ref_mk{8400};
-    double demand_ref_rd{3150};
-    double demand_pow_price{1};
-    double demand_pow_mk{0.5};
-    double demand_pow_rd{1};
+    double demand_ref_price {30};
+    double demand_ref_mk {8400};
+    double demand_ref_rd {3150};
+    double demand_pow_price {1};
+    double demand_pow_mk {0.5};
+    double demand_pow_rd {1};
 
-    double share_price{0.4}; // 0.01 * setting
-    double share_mk{0.3}; // 0.01 * setting
-    double share_rd{0.3}; // 0.01 * setting
-    double share_pow_price{3};
-    double share_pow_mk{1.5};
-    double share_pow_rd{1};
+    double share_price {0.4}; // 0.01 * setting
+    double share_mk {0.3}; // 0.01 * setting
+    double share_rd {0.3}; // 0.01 * setting
+    double share_pow_price {3};
+    double share_pow_mk {1.5};
+    double share_pow_rd {1};
 
-    double price_overload{40};
+    double price_overload {40};
 
     // mpi related
 
-    double mpi_retern_factor{11155};
-    double mpi_factor_a{50};
-    double mpi_factor_b{10};
-    double mpi_factor_c{10};
-    double mpi_factor_d{10};
-    double mpi_factor_e{10};
-    double mpi_factor_f{10};
+    double mpi_retern_factor {11155};
+    double mpi_factor_a {50};
+    double mpi_factor_b {10};
+    double mpi_factor_c {10};
+    double mpi_factor_d {10};
+    double mpi_factor_e {10};
+    double mpi_factor_f {10};
 };
 
 struct Decision {
-    double price[MAX_PLAYER];
-    double prod[MAX_PLAYER];
-    double mk[MAX_PLAYER];
-    double ci[MAX_PLAYER];
-    double rd[MAX_PLAYER];
+    MESE_ARR(price);
+    MESE_ARR(prod);
+    MESE_ARR(mk);
+    MESE_ARR(ci);
+    MESE_ARR(rd);
 };
 
 struct PeriodDataEarly {
-    double prod_rate[MAX_PLAYER];
-    double prod_over[MAX_PLAYER];
-    double prod_cost_unit[MAX_PLAYER];
-    double prod_cost_marginal[MAX_PLAYER];
-    double prod_cost[MAX_PLAYER];
+    MESE_ARR(prod_rate);
+    MESE_ARR(prod_over);
+    MESE_ARR(prod_cost_unit);
+    MESE_ARR(prod_cost_marginal);
+    MESE_ARR(prod_cost);
 
-    double deprecation[MAX_PLAYER];
-    double capital[MAX_PLAYER]; // *
-    double size[MAX_PLAYER]; // *
-    double spending[MAX_PLAYER];
-    double balance_early[MAX_PLAYER];
-    double loan_early[MAX_PLAYER];
-    double interest[MAX_PLAYER];
+    MESE_ARR(deprecation);
+    MESE_ARR(capital); // *
+    MESE_ARR(size); // *
+    MESE_ARR(spending);
+    MESE_ARR(balance_early);
+    MESE_ARR(loan_early);
+    MESE_ARR(interest);
 
-    double goods[MAX_PLAYER];
-    double goods_cost[MAX_PLAYER];
-    double goods_max_sales[MAX_PLAYER];
+    MESE_ARR(goods);
+    MESE_ARR(goods_cost);
+    MESE_ARR(goods_max_sales);
 
-    double history_mk[MAX_PLAYER]; // *
-    double history_rd[MAX_PLAYER]; // *
+    MESE_ARR(history_mk); // *
+    MESE_ARR(history_rd); // *
 };
 
 struct PeriodData {
-    double average_price_given;
-    double average_price_planned;
-    double average_price_mixed;
-    double demand_effect_mk;
-    double demand_effect_rd;
-    double orders_demand;
+    MESE_VAL(average_price_given);
+    MESE_VAL(average_price_planned);
+    MESE_VAL(average_price_mixed);
+    MESE_VAL(demand_effect_mk);
+    MESE_VAL(demand_effect_rd);
+    MESE_VAL(orders_demand);
 
-    double share_effect_price[MAX_PLAYER];
-    double share_effect_mk[MAX_PLAYER];
-    double share_effect_rd[MAX_PLAYER];
-    double share[MAX_PLAYER];
-    double share_compressed[MAX_PLAYER];
+    MESE_ARR(share_effect_price);
+    MESE_ARR(share_effect_mk);
+    MESE_ARR(share_effect_rd);
+    MESE_ARR(share);
+    MESE_ARR(share_compressed);
 
-    double orders[MAX_PLAYER];
-    double sold[MAX_PLAYER];
-    double inventory[MAX_PLAYER]; // *
-    double unfilled[MAX_PLAYER];
+    MESE_ARR(orders);
+    MESE_ARR(sold);
+    MESE_ARR(inventory); // *
+    MESE_ARR(unfilled);
 
-    double goods_cost_sold[MAX_PLAYER];
-    double goods_cost_inventory[MAX_PLAYER]; // *
+    MESE_ARR(goods_cost_sold);
+    MESE_ARR(goods_cost_inventory); // *
 
-    double sales[MAX_PLAYER]; // *
-    double inventory_charge[MAX_PLAYER];
-    double cost_before_tax[MAX_PLAYER];
-    double profit_before_tax[MAX_PLAYER];
-    double tax_charge[MAX_PLAYER];
-    double profit[MAX_PLAYER];
+    MESE_ARR(sales); // *
+    MESE_ARR(inventory_charge);
+    MESE_ARR(cost_before_tax);
+    MESE_ARR(profit_before_tax);
+    MESE_ARR(tax_charge);
+    MESE_ARR(profit);
 
-    double balance[MAX_PLAYER];
-    double loan[MAX_PLAYER]; // *
-    double cash[MAX_PLAYER]; // *
-    double retern[MAX_PLAYER]; // *
+    MESE_ARR(balance);
+    MESE_ARR(loan); // *
+    MESE_ARR(cash); // *
+    MESE_ARR(retern); // *
 
-    double average_price; // *
+    MESE_VAL(average_price); // *
 
-    double mpi_a[MAX_PLAYER];
-    double mpi_b[MAX_PLAYER];
-    double mpi_c[MAX_PLAYER];
-    double mpi_d[MAX_PLAYER];
-    double mpi_e[MAX_PLAYER];
-    double mpi_f[MAX_PLAYER];
-    double mpi[MAX_PLAYER];
+    MESE_ARR(mpi_a);
+    MESE_ARR(mpi_b);
+    MESE_ARR(mpi_c);
+    MESE_ARR(mpi_d);
+    MESE_ARR(mpi_e);
+    MESE_ARR(mpi_f);
+    MESE_ARR(mpi);
 };
 
 class Game;
