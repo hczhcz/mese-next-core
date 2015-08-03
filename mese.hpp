da474e88c7d0ac50fede91eaa4ce3ba27c3b6eeb
hcz <h.cz@qq.com>
2015-08-03 21:02:49 +0800

    move function print

--- mese.hpp
+++ mese.hpp
@@ -7,13 +7,13 @@
 
 namespace mese {
 
+const size_t MAX_PLAYER {16};
+
 #define MESE_VAL(name) double name {NAN}
-#define MESE_ARR(name) double name[16] { \
+#define MESE_ARR(name) double name[MAX_PLAYER] { \
     NAN, NAN, NAN, NAN, NAN, NAN, NAN, NAN, \
     NAN, NAN, NAN, NAN, NAN, NAN, NAN, NAN, \
 }
-
-const size_t MAX_PLAYER {16};
 
 // #define MESE_CLASSIC
 #ifdef MESE_CLASSIC
@@ -234,7 +234,7 @@ class Game;
 
 class Period: public PeriodDataEarly, public PeriodData {
 private:
-    inline double sum(double (&member)[MAX_PLAYER]) {
+    inline double sum(double *member) {
         double result = 0;
 
         for (size_t i = 0; i < player_count; ++i) {
@@ -242,75 +242,6 @@ private:
         }
 
         return result;
-    }
-
-    template <class T>
-    void print(std::ostream &stream, T callback) {
-        size_t indent {0};
-
-        auto print_indent = [&]() {
-            stream << std::endl;
-            for (size_t i = 0; i < indent; ++i) {
-                stream << "    ";
-            }
-        };
-
-        auto val_handler = [&](double value) {
-            stream << value;
-        };
-
-        auto arr_handler = [&](double (&member)[MAX_PLAYER]) {
-            stream << '[' << member[0];
-
-            for (size_t i = 1; i < player_count; ++i) {
-                stream << ", " << member[i];
-            }
-
-            stream << ']';
-        };
-
-        auto doc_handler = [&](auto self, auto callback) {
-            stream << '{';
-            ++indent;
-
-            auto m_val_handler = [&](
-                const std::string &name,
-                double value
-            ) {
-                print_indent();
-                stream << name << ": ";
-                val_handler(value);
-                stream << ',';
-            };
-
-            auto m_arr_handler = [&](
-                const std::string &name,
-                double (&member)[MAX_PLAYER]
-            ) {
-                print_indent();
-                stream << name << ": ";
-                arr_handler(member);
-                stream << ',';
-            };
-
-            auto m_doc_handler = [&](
-                const std::string &name,
-                auto callback
-            ) {
-                print_indent();
-                stream << name << ": ";
-                self(self, callback);
-                stream << ',';
-            };
-
-            callback(m_val_handler, m_arr_handler, m_doc_handler);
-
-            --indent;
-            print_indent();
-            stream << '}';
-        };
-
-        doc_handler(doc_handler, callback);
     }
 
 public:
--- util_print.hpp
+++ util_print.hpp
@@ -5,4 +5,73 @@
 
 namespace mese {
 
+template <class T>
+void print(std::ostream &stream, size_t arr_size, T callback) {
+    size_t indent {0};
+
+    auto print_indent = [&]() {
+        stream << std::endl;
+        for (size_t i = 0; i < indent; ++i) {
+            stream << "    ";
+        }
+    };
+
+    auto val_handler = [&](double value) {
+        stream << value;
+    };
+
+    auto arr_handler = [&](double *member) {
+        stream << '[' << member[0];
+
+        for (size_t i = 1; i < arr_size; ++i) {
+            stream << ", " << member[i];
+        }
+
+        stream << ']';
+    };
+
+    auto doc_handler = [&](auto self, auto callback) {
+        stream << '{';
+        ++indent;
+
+        auto m_val_handler = [&](
+            const std::string &name,
+            double value
+        ) {
+            print_indent();
+            stream << name << ": ";
+            val_handler(value);
+            stream << ',';
+        };
+
+        auto m_arr_handler = [&](
+            const std::string &name,
+            double *member
+        ) {
+            print_indent();
+            stream << name << ": ";
+            arr_handler(member);
+            stream << ',';
+        };
+
+        auto m_doc_handler = [&](
+            const std::string &name,
+            auto callback
+        ) {
+            print_indent();
+            stream << name << ": ";
+            self(self, callback);
+            stream << ',';
+        };
+
+        callback(m_val_handler, m_arr_handler, m_doc_handler);
+
+        --indent;
+        print_indent();
+        stream << '}';
+    };
+
+    doc_handler(doc_handler, callback);
+}
+
 }
